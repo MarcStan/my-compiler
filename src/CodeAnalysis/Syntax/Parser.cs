6829e8ac248e599fcf3189459615610d98571460
Marc Stan <marcstan@live.com>
2019-12-22 16:13:45 +0100

    fix bug with variables not being set within expression

--- src/CodeAnalysis/Syntax/Parser.cs
+++ src/CodeAnalysis/Syntax/Parser.cs
@@ -118,7 +118,7 @@ namespace CodeAnalysis.Syntax
                 case SyntaxKind.OpenParenthesisToken:
                     {
                         var left = GetNextToken();
-                        var expression = ParseBinaryExpression();
+                        var expression = ParseExpression();
                         var right = MatchToken(SyntaxKind.CloseParenthesisToken);
                         return new ParenthesizedExpressionSyntax(left, expression, right);
                     }
